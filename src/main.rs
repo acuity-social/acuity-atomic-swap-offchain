f7c8b5ca8cc8ca0281a2c491576825a9c245c5ea
Jonathan Brown <jbrown@acuity.social>
2021-07-08 15:34:03 +0700

    open database

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use rocksdb::DB;
 use web3::futures::{future, StreamExt};
 
 #[tokio::main]
 async fn main() -> web3::Result {
+    let path = "database";
+    let _db = DB::open_default(path).unwrap();
+
     let ws = web3::transports::WebSocket::new("wss://mainnet.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161").await?;
     let web3 = web3::Web3::new(ws.clone());
     let mut sub = web3.eth_subscribe().subscribe_new_heads().await?;
