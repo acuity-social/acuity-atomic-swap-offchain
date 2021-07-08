23756d459fe705c3e9074403bd80f5d6e1ed9d0d
Jonathan Brown <jbrown@acuity.social>
2021-07-08 15:51:33 +0700

    Connect to Acuity.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use rocksdb::DB;
+use substrate_api_client::{Api, Metadata};
+use keyring::AccountKeyring;
 use web3::futures::{future, StreamExt};
 
 #[tokio::main]
 async fn main() -> web3::Result {
     let path = "database";
     let _db = DB::open_default(path).unwrap();
+
+    let url = "127.0.0.1:9946";
+    let signer = AccountKeyring::Alice.pair();
+
+     let api = Api::new(format!("ws://{}", url))
+         .map(|api| api.set_signer(signer.clone()))
+         .unwrap();
+
+    // print full substrate metadata json formatted
+     println!(
+         "{}",
+         Metadata::pretty_format(&api.get_metadata().unwrap())
+             .unwrap_or_else(|| "pretty format failed".to_string())
+     );
 
     let ws = web3::transports::WebSocket::new("wss://mainnet.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161").await?;
     let web3 = web3::Web3::new(ws.clone());
