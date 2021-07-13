18f8c24a3f6db19f9c1d83e7e7a0c4c4674dd42c
Jonathan Brown <jbrown@acuity.social>
2021-07-14 03:56:15 +0700

    Use substrate-subxt instead of substrate-api-client

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,38 @@
-use std::sync::mpsc::channel;
 use rocksdb::DB;
-use substrate_api_client::utils::FromHexString;
-use substrate_api_client::{Api, Metadata};
-use substrate_api_client::events::EventsDecoder;
-use keyring::AccountKeyring;
 use web3::futures::{future, StreamExt};
-use sp_runtime::AccountId32 as AccountId;
-use codec::{Decode, Encode};
-use log::{debug, error};
-use std::convert::TryFrom;
-
-#[derive(Decode)]
-struct AddToOrderEventArgs {
-    seller: AccountId,
-    asset_id: [u8; 16],
-    price: u128,
-    foreign_address: [u8; 32],
-    value: u128,
-}
+use substrate_subxt::{
+    balances::{
+        TransferEvent,
+    },
+    sp_core::Decode,
+    ClientBuilder,
+    DefaultNodeRuntime,
+    EventSubscription,
+};
 
 #[tokio::main]
 async fn main() {
     let path = "database";
     let _db = DB::open_default(path).unwrap();
 
-    let url = "127.0.0.1:9946";
-    let signer = AccountKeyring::Alice.pair();
+    let client = ClientBuilder::<DefaultNodeRuntime>::new()
+        .register_type_size::<[u8; 32]>("[u8; 32]")
+        .register_type_size::<[u8; 16]>("[u8; 16]")
+        .register_type_size::<u64>("Timestamp")
+        .register_type_size::<[u8; 20]>("EthereumAddress")
+        .set_url("ws://127.0.0.1:9946").build().await.unwrap();
 
-     let api = Api::new(format!("ws://{}", url))
-         .map(|api| api.set_signer(signer.clone()))
-         .unwrap();
-/*
-    // print full substrate metadata json formatted
-     println!(
-         "{}",
-         Metadata::pretty_format(&api.get_metadata().unwrap())
-             .unwrap_or_else(|| "pretty format failed".to_string())
-     );
-*/
-    println!("Subscribe to events");
-    let (events_in, events_out) = channel();
-
-    api.subscribe_events(events_in).unwrap();
-
-    let event_decoder = match None {
-        Some(d) => d,
-        None => EventsDecoder::try_from(Metadata::try_from(api.get_metadata().unwrap()).unwrap()).unwrap(),
-    };
-
-    loop {
-        let event_str = events_out.recv().unwrap();
-        let _events = event_decoder.decode_events(&mut Vec::from_hex(event_str).unwrap().as_slice());
-        println!("wait for raw event");
-        match _events {
-            Ok(raw_events) => {
-                for (phase, event) in raw_events.into_iter() {
-                    println!("Decoded Event: {:?}, {:?}", phase, event);
-/*                    match event {
-                        RuntimeEvent::Raw(raw)
-                            if raw.module == module && raw.variant == variant =>
-                        {
-                            return Ok(raw);
-                        }
-                        _ => println!("ignoring unsupported module event: {:?}", event),
-                    }
-*/                }
-            }
-            Err(_) => println!("couldn't decode event record list"),
-        }
+    let sub = client.subscribe_events().await.unwrap();
+    let decoder = client.events_decoder();
+    let mut sub = EventSubscription::<DefaultNodeRuntime>::new(sub, decoder);
+    sub.filter_event::<TransferEvent<_>>();
+    let raw = sub.next().await.unwrap().unwrap();
+    let event = TransferEvent::<DefaultNodeRuntime>::decode(&mut &raw.data[..]);
+    if let Ok(e) = event {
+        println!("Balance transfer success: value: {:?}", e.amount);
+    } else {
+        println!("Failed to subscribe to Balances::Transfer Event");
     }
-
-/*
-    let args: AddToOrderEventArgs = api
-        .wait_for_event("AtomicSwap", "AddToOrder", None, &events_out)
-        .unwrap();
-
-    println!("Seller: {:?}", args.seller);
-    println!("AssetId: {:?}", args.asset_id);
-    println!("Price: {:?}", args.price);
-    println!("Foreign address: {:?}", args.foreign_address);
-    println!("Value: {:?}", args.value);
-*/
-
-
-
 
     let ws = web3::transports::WebSocket::new("wss://mainnet.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161").await.unwrap();
     let web3 = web3::Web3::new(ws);
@@ -97,5 +46,4 @@ async fn main() {
             future::ready(())
         })
         .await;
-
 }
