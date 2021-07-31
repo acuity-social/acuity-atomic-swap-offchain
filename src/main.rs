38c037fba5e3e1ef521a0ea16d19338fe1d037e4
Jonathan Brown <jbrown@acuity.social>
2021-07-31 21:20:27 +0700

    Catch AddToOrderEvent

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,52 @@ use rocksdb::DB;
 use web3::futures::{future, StreamExt};
 use substrate_subxt::{
     balances::{
-        TransferEvent,
+        AccountData,
+        Balances,
+        BalancesEventTypeRegistry,
+    },
+    session::{
+        Session,
+        SessionEventTypeRegistry,
+    },
+    staking::{
+        Staking,
+        StakingEventTypeRegistry,
+    },
+    sudo::{
+        Sudo,
+        SudoEventTypeRegistry,
+    },
+    system::{
+        System,
+        SystemEventTypeRegistry,
     },
     ClientBuilder,
-    DefaultNodeRuntime,
     EventSubscription,
-    system::System,
     sp_runtime::traits::{
         AtLeast32Bit,
         MaybeSerialize,
         Member,
     },
+    EventTypeRegistry,
+    extrinsic::{
+        DefaultExtra,
+    },
+    BasicSessionKeys,
+    Runtime,
 };
 use std::fmt::Debug;
 
+use sp_runtime::{
+    generic::Header,
+    traits::{
+        BlakeTwo256,
+        IdentifyAccount,
+        Verify,
+    },
+    MultiSignature,
+    OpaqueExtrinsic,
+};
 
 use codec::{
     Codec,
@@ -23,6 +55,54 @@ use codec::{
 };
 
 use proc_macro::*;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct AcuityRuntime;
+
+impl Staking for AcuityRuntime {}
+
+impl Runtime for AcuityRuntime {
+    type Signature = MultiSignature;
+    type Extra = DefaultExtra<Self>;
+
+    fn register_type_sizes(event_type_registry: &mut EventTypeRegistry<Self>) {
+        event_type_registry.with_system();
+        event_type_registry.with_balances();
+        event_type_registry.with_session();
+        event_type_registry.with_staking();
+        event_type_registry.with_sudo();
+        substrate_subxt::register_default_type_sizes(event_type_registry);
+    }
+}
+
+impl System for AcuityRuntime {
+    type Index = u32;
+    type BlockNumber = u32;
+    type Hash = sp_core::H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = <<MultiSignature as Verify>::Signer as IdentifyAccount>::AccountId;
+    type Address = sp_runtime::MultiAddress<Self::AccountId, u32>;
+    type Header = Header<Self::BlockNumber, BlakeTwo256>;
+    type Extrinsic = OpaqueExtrinsic;
+    type AccountData = AccountData<<Self as Balances>::Balance>;
+}
+
+impl Balances for AcuityRuntime {
+    type Balance = u128;
+}
+
+impl Session for AcuityRuntime {
+    type ValidatorId = <Self as System>::AccountId;
+    type Keys = BasicSessionKeys;
+}
+
+impl Sudo for AcuityRuntime {}
+
+impl AtomicSwap for AcuityRuntime {
+    type Balance = u128;
+}
+
+
 
 #[module]
 pub trait AtomicSwap: System {
@@ -51,7 +131,7 @@ async fn main() {
     let path = "database";
     let _db = DB::open_default(path).unwrap();
 
-    let client = ClientBuilder::<DefaultNodeRuntime>::new()
+    let client = ClientBuilder::<AcuityRuntime>::new()
         .register_type_size::<[u8; 16]>("AcuityOrderId")
         .register_type_size::<[u8; 16]>("AcuityAssetId")
         .register_type_size::<[u8; 32]>("AcuityForeignAddress")
@@ -63,12 +143,16 @@ async fn main() {
 
     let sub = client.subscribe_events().await.unwrap();
     let decoder = client.events_decoder();
-    let mut sub = EventSubscription::<DefaultNodeRuntime>::new(sub, decoder);
-    sub.filter_event::<TransferEvent<_>>();
+    let mut sub = EventSubscription::<AcuityRuntime>::new(sub, decoder);
+    sub.filter_event::<AddToOrderEvent<_>>();
     let raw = sub.next().await.unwrap().unwrap();
-    let event = TransferEvent::<DefaultNodeRuntime>::decode(&mut &raw.data[..]);
+    let event = AddToOrderEvent::<AcuityRuntime>::decode(&mut &raw.data[..]);
     if let Ok(e) = event {
-        println!("Balance transfer success: value: {:?}", e.amount);
+        println!("Balance transfer success: seller: {:?}", e.seller);
+        println!("Balance transfer success: asset_id: {:?}", e.asset_id);
+        println!("Balance transfer success: price: {:?}", e.price);
+        println!("Balance transfer success: foreign_address: {:?}", e.foreign_address);
+        println!("Balance transfer success: value: {:?}", e.value);
     } else {
         println!("Failed to subscribe to Balances::Transfer Event");
     }
